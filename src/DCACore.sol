772edb4d48ee1d53fc5c7d17851a7994d6559895
Parth Gupta <parth.eng1210@gmail.com>
2024-11-04 03:30:06 +0530

    feat: add scheduledCCTP

--- src/DCACore.sol
+++ src/DCACore.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.25;
 
-import "@chainlink/contracts/src/v0.8/automation/interfaces/KeeperCompatibleInterface.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "./CCTPTransfer.sol";  
 
-contract DCACore is KeeperCompatibleInterface {
+import "wormhole-solidity-sdk/interfaces/IERC20.sol";
+
+contract DCACore is AutomationCompatibleInterface {
     struct DCAInStrategy {
         address user;
         address[] inTokens;
@@ -21,6 +24,19 @@ contract DCACore is KeeperCompatibleInterface {
         uint256 triggerPrice;
     }
 
+    struct CrossChainTransferTrigger {
+        address user;
+        uint256 amount;
+        uint16 targetChain;
+        address triggerToken;
+        address targetAddress;
+        address recipient;
+        uint256 triggerPrice; // 10^8
+    }
+
+    mapping(address => CrossChainTransferTrigger) public crossChainTransferTriggers;
+    address[] public crossChainUsers;
+
     mapping(address => DCAInStrategy) public dcaInStrategies;
     mapping(address => DCAOutStrategy) public dcaOutStrategies;
 
@@ -29,6 +45,10 @@ contract DCACore is KeeperCompatibleInterface {
 
     // Chainlink Price Feed addresses for trigger tokens
     mapping(address => AggregatorV3Interface) public priceFeeds;
+
+    CCTPTransfer public cctpTransferContract;
+
+    address private USDC;
 
     event DCAInTriggered(address indexed user);
     event DCAOutTriggered(address indexed user);
@@ -39,12 +59,18 @@ contract DCACore is KeeperCompatibleInterface {
         address linkPriceFeed, 
         address WETH, 
         address WBTC, 
-        address LINK
+        address LINK,
+        address _USDC,
+        address _cctpTransferAddress
+
     ) {
         // Set Chainlink price feeds for tokens
         priceFeeds[WETH] = AggregatorV3Interface(ethPriceFeed);
         priceFeeds[WBTC] = AggregatorV3Interface(wbtcPriceFeed);
         priceFeeds[LINK] = AggregatorV3Interface(linkPriceFeed);
+
+        cctpTransferContract = CCTPTransfer(_cctpTransferAddress);
+        USDC = _USDC;
     }
 
     function setDCAIn(
@@ -88,6 +114,30 @@ contract DCACore is KeeperCompatibleInterface {
         });
     }
 
+    function setCrossChainTransferTrigger(
+        address user,
+        uint16 targetChain,
+        address triggerToken,
+        address targetAddress,
+        address recipient,
+        uint256 amount,
+        uint256 triggerPrice
+    ) external {
+        if (crossChainTransferTriggers[user].user == address(0)) {
+            crossChainUsers.push(user); // Add new user if not already present
+        }
+
+        crossChainTransferTriggers[user] = CrossChainTransferTrigger({
+            user: user,
+            targetChain: targetChain,
+            triggerToken: triggerToken,
+            targetAddress: targetAddress,
+            recipient: recipient,
+            amount: amount,
+            triggerPrice: triggerPrice
+        });
+    }
+
     // Chainlink Keeper's checkUpkeep function to determine if action is needed
     function checkUpkeep(bytes calldata) external view override returns (bool upkeepNeeded, bytes memory) {
         for (uint256 i = 0; i < dcaUsers.length; i++) {
@@ -101,6 +151,14 @@ contract DCACore is KeeperCompatibleInterface {
                 return (true, abi.encode(user, "dcaOut"));
             }
         }
+        for (uint256 i = 0; i < crossChainUsers.length; i++) {
+            address user = crossChainUsers[i];
+            
+            if (_shouldTriggerCrossChainTransfer(user)) {
+                return (true, abi.encode(user, "crossChainTransfer"));
+            }
+        }
+
 
         upkeepNeeded = false;
     }
@@ -113,6 +171,8 @@ contract DCACore is KeeperCompatibleInterface {
             _triggerDCAIn(user);
         } else if (keccak256(abi.encodePacked(action)) == keccak256("dcaOut")) {
             _triggerDCAOut(user);
+        } else if (keccak256(abi.encodePacked(action)) == keccak256("crossChainTransfer")) {
+            _triggerCrossChainTransfer(user);
         }
     }
 
@@ -135,10 +195,42 @@ contract DCACore is KeeperCompatibleInterface {
         return currentPrice >= int256(strategy.triggerPrice);
     }
 
+     function _shouldTriggerCrossChainTransfer(address user) internal view returns (bool) {
+        CrossChainTransferTrigger storage trigger = crossChainTransferTriggers[user];
+        AggregatorV3Interface priceFeed = priceFeeds[trigger.triggerToken];
+        
+        if (address(priceFeed) == address(0)) return false;
+
+        (, int256 currentPrice, , , ) = priceFeed.latestRoundData();
+        return currentPrice >= int256(trigger.triggerPrice);
+    }
+
+    function _triggerCrossChainTransfer(address user) internal {
+        CrossChainTransferTrigger storage trigger = crossChainTransferTriggers[user];
+        uint256 cost = cctpTransferContract.quoteCrossChainDeposit(trigger.targetChain);
+
+        IERC20(USDC).transferFrom(user, address(this), trigger.amount);
+        IERC20(USDC).approve(trigger.targetAddress, trigger.amount);
+
+        // Initiate cross-chain transfer using CCTPTransfer contract
+        cctpTransferContract.sendCrossChainDeposit{value: cost}(
+            trigger.targetChain,
+            trigger.targetAddress,
+            trigger.recipient,
+            trigger.amount  // This could be replaced with a specific USDC amount
+        );
+    }
+
+
     function _triggerDCAOut(address user) internal {
         emit DCAOutTriggered(user);
     }
 
+    // Retrieve all users with cross-chain transfer triggers
+    function getCrossChainUsers() external view returns (address[] memory) {
+        return crossChainUsers;
+    }
+    
     // Function to retrieve all DCA users
     function getDCAUsers() external view returns (address[] memory) {
         return dcaUsers;
